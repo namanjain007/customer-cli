a12d10eca1e0dbc14cab062e55bce70210024a67
Naman Jain <namandude1008@gmail.com>
2020-11-02 22:31:35 +0530

    added inquirer

--- commands.js
+++ commands.js
@@ -1,18 +1,56 @@
 const { program } = require('commander');
+const { prompt } = require('inquirer');
+
 const { addCustomer , findCustomer} = require('./index');
+
+const questions = [
+    {
+        type : 'input',
+        name : 'fname',
+        message : 'customer first name'
+    },
+    {
+        type : 'input',
+        name : 'lname',
+        message : 'customer last name'
+    },
+    {
+        type : 'input',
+        name : 'phone',
+        message : 'customer phone number'
+    },
+    {
+        type : 'input',
+        name : 'email',
+        message : 'customer email'
+    }
+];
 
 program
     .version('1.0.0')
     .description('Customer Management System')
 
-program
-    .command('add <fname> <lname> <phone> <email>')
-    .alias('a')
-    .description('add new customer command')
-    .action((fname, lname, phone, email) => {
-        addCustomer({fname, lname, phone, email});
-    })
+// program
+//     .command('add <fname> <lname> <phone> <email>')
+//     .alias('a')
+//     .description('add new customer command')
+//     .action((fname, lname, phone, email) => {
+//         addCustomer({fname, lname, phone, email});
+//     })
 
+program
+    .command('add')
+    .alias('a')
+    .description('Adding new Customer into database')
+    .action(() => {
+        prompt(questions)
+            .then(answers => {
+                addCustomer(answers);
+            })
+            .catch(err => {
+                throw err;
+            })
+    })
 
 program
     .command('find <fname>')
--- index.js
+++ index.js
@@ -8,13 +8,13 @@ mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true} );
 
 const db = mongoose.connection;
 
-db.once('open', _ => {
-    console.log('Database connected:', url)
-  })
-  
-  db.on('error', err => {
-    console.error('connection error:', err)
-  })
+// db.once('open', _ => {
+//     console.log('Database connected:', url)
+//   })
+
+//   db.on('error', err => {
+//     console.error('connection error:', err)
+//   })
 
 const addCustomer = (customer) => {
     Customer.create(customer).then(customer => {
